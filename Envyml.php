1cf0868d0b249f1c48f75a6ca9d61d66515c689f
Benjamin <brannow@users.noreply.github.com>
2019-10-22 16:17:37 +0200

    Update Envyml.php

--- Envyml.php
+++ Envyml.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Symfony\Component\Dotenv;
+namespace Symfony\Component\Envyml;
 
 use Symfony\Component\Dotenv\Exception\FormatException;
 use Symfony\Component\Dotenv\Exception\FormatExceptionContext;
